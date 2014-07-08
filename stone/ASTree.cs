a8150bd40403226de83b4baa5f254e79759d1f4a
cyendra <g1483335912@gmail.com>
2014-07-08 20:47:01 +0800

    a

---
+++ stone/ASTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stone
+{
+    class ASTree
+    {
+        public abstract ASTree child(int i);
+        public abstract int NumChildren();
+        public abstract string location();
+    }
+}
